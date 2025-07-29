b32d59c062d6e7902dd77fe7a443ca31ebd03a07
zetta <sethtemp0@outlook.com>
2025-07-29 13:46:20 -0500

    added sharpcollection

--- sources/gitinstalls.sh
+++ sources/gitinstalls.sh
@@ -4,6 +4,10 @@ pwndbg(){
 	git clone https://github.com/pwndbg/pwndbg && cd pwndbg && ./setup.sh
 }
 
+sharpcolleciton(){
+	git clone https://github.com/Flangvik/SharpCollection.git /opt/SharpCollection
+
 gitinstall(){
 	pwndbg
+	sharpcollection
 }
